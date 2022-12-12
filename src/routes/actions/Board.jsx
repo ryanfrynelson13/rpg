cea4fe783f492977dc243bad4f0694b3ce251c10
ryanfrynelson <ryanfrynelson@hotmail.fr>
2022-12-12 19:08:26 +0100

    finished map movement

--- src/routes/actions/Board.jsx
+++ src/routes/actions/Board.jsx
@@ -9,6 +9,8 @@ const Board = () => {
 
   const [boardSquares, setBoardSquares] = useState([])
   const [currentSquare, setCurrentSquare] = useState(85)
+  const [positionX, setPositionX] = useState(30)
+  const [positionY, setPositionY] = useState(30)
 
   useEffect(()=> {
     divElement.current.focus()
@@ -21,20 +23,36 @@ const Board = () => {
 
   const moveHero = (event) => {
     if(event.key === 'ArrowRight' ){
-      setCurrentSquare(currentSquare +1)
-      setBoardSquares(boardSquares)
+      if(currentSquare % 20 === 19 && positionX < 100){
+        setPositionX(positionX +5)
+      } else if(currentSquare % 20 !== 0) {
+        setCurrentSquare(currentSquare +1)
+        setBoardSquares(boardSquares)
+      }
     }
     if(event.key === 'ArrowLeft' ){
-      setCurrentSquare(currentSquare -1)
-      setBoardSquares(boardSquares)
+      if(currentSquare % 20 === 2 && positionX > 0){
+        setPositionX(positionX -5)
+      } else if(currentSquare % 20 !== 1){
+        setCurrentSquare(currentSquare -1)
+        setBoardSquares(boardSquares)
+      }
     }
     if(event.key === 'ArrowUp' ){
-      setCurrentSquare(currentSquare -20)
-      setBoardSquares(boardSquares)
+      if(currentSquare <= 40 && positionY > 0) {
+        setPositionY(positionY - 5)
+      } else if(currentSquare > 20){
+        setCurrentSquare(currentSquare -20)
+        setBoardSquares(boardSquares)
+      }
     }
     if(event.key === 'ArrowDown' ){
-      setCurrentSquare(currentSquare +20)
-      setBoardSquares(boardSquares)
+      if(currentSquare > 160 && positionY < 100 ){
+        setPositionY(positionY + 5)
+      } else if (currentSquare <= 180){
+        setCurrentSquare(currentSquare +20)
+        setBoardSquares(boardSquares)
+      }
     }
   } 
   return (
@@ -42,13 +60,13 @@ const Board = () => {
         <h2 className='text-center'>Board</h2>
         <div className='board' style={{  
             backgroundImage: `url(${swamp})`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
+            backgroundPosition: `${positionX}% ${positionY}%`,            
+            backgroundSize: '150%',
             backgroundRepeat: 'no-repeat'
         }}>
             {
               boardSquares.map((el, i) => {
-                let curr = i === currentSquare
+                let curr = i+1 === currentSquare
                 return (
                   <div key={i} className='board-square'>
                     <BoardSquare curr={curr}/>
